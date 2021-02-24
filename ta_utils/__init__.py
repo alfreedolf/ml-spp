69b638c1ec08940223cfa12f335b4d685496bb16
alfreedolf <2171254+alfreedolf@users.noreply.github.com>
2021-02-25 00:39:18 +0100

    feat: BenchmarkModel metrics analysis: MSE

--- ta_utils/__init__.py
+++ ta_utils/__init__.py
@@ -1,4 +1,6 @@
 # this package implements functions that returns
 # technical analysis indicators
 from .technical_indicators import bollinger_bands
+from .tecnhical_indicators import std_dev
+from .tecnhical_indicators import volatility
 from .tecnhical_indicators import moving_average
