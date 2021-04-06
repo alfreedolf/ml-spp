acdb8fbb9254770c5a6a693604a863f47eb6b758
alfreedolf <2171254+alfreedolf@users.noreply.github.com>
2021-04-06 23:31:44 +0200

    feat: fix to S3 data retrieval code

--- source_deepar/deepar_lambda_fucntion.py
+++ source_deepar/deepar_lambda_fucntion.py
@@ -3,6 +3,7 @@
 import boto3
 # we need to use json in order to interact with endpoint I/O
 import json
+import os
 
 
 def lambda_handler(event, context):
@@ -11,19 +12,21 @@ def lambda_handler(event, context):
     sagemaker = boto3.client('sagemaker')
     sagemaker_session = sagemaker.Session()
     # S3 resource invocation
-    s3 = boto3.resource('s3')
+    resource = boto3.resource('s3')
     # S3 bucket selection
-    # TODO check if it's possible to use default bucket here, if not, select the proper bucket
-    bucket = sagemaker_session.default_bucket()
+
+    bucket = "stock-prediction-data-4327a669-7f13-48c7-aa4a-49a80b9e1e32"
 
     # The SageMaker runtime is what allows us to invoke the endpoint that we've created.
     runtime = boto3.Session().client('sagemaker-runtime')
 
+    # >> Something
+    json_data = get_json_stock_data(event['ticker_name'], resource, bucket)
     # Now we use the SageMaker runtime to invoke our endpoint, sending the review we were given
-    response = runtime.invoke_endpoint(EndpointName='***ENDPOINT NAME HERE***',  # The name of the endpoint we created
+    response = runtime.invoke_endpoint(EndpointName='DeepAR-ml-spp',  # The name of the endpoint we created
                                        ContentType='application/json',  # The data format that is expected
-                                       Body=encode_request(event['ticker_name'],
-                                                           event['start_date']))  # The actual review
+                                       Body=encode_request(json_data,
+                                                           event['start_date']))  # The actual prediction requeste
 
     # The response is an HTTP response whose body contains the result of our inference
     result = response['Body'].read().decode('utf-8')
@@ -38,15 +41,16 @@ def lambda_handler(event, context):
     }
 
 
-def encode_request(ticker_name, start_date):
+def encode_request(ticker_name, start_date, s3_path):
     """
     Encodes a request to be fed to the SageMaker endpoing
+    :param s3_path: where to find json data on S3
     :param ticker_name: a string indicating which stock has to be predicted.
                         Possible values: 'IBM', 'AAPL', 'AMZN', 'GOOGL'.
     :param start_date: start date from which to predict from
-    :return: a json object containing a request ready to be sent to the endpoin
+    :return: a json object containing a request ready to be sent to the endpoint
     """
-    instances = [ticker_name2_json_obj(ticker_name, start=start_date)]
+    instances = [get_json_stock_data(ticker_name, start_date, s3_path)]
     configuration = {
         "num_samples": 100,
         "output_types": ["quantiles"],
@@ -56,48 +60,86 @@ def encode_request(ticker_name, start_date):
     return json.dumps(http_request_data).encode("utf-8")
 
 
-def ticker_name2_json_obj(ticker_name, start):
+def get_ibm_json_from_s3(s3_resource, bucket_name, prefix=''):
     """
-    Returns a json object containing the data relative to ticker_name and start date
-    :param ticker_name:
-    :param start:
-    :return: json object containing data from
+    Load IBM stock json data from S3 resource
+    :param prefix: prefix of the path where the file is located
+    :param s3_resource: s3 resource to get data from
+    :param bucket_name: s3 bucket to get data from
+    :return: json object containing IBM stock price adjusted close
     """
-    # get start time and target from the time series, ts
-    if start is not None:
-        start_index = start
-        ts = ts.loc[start_index:]
+    ibm_filename = "IBM.json"
+    complete_path = os.path.join(prefix, ibm_filename)
+    json_object = s3_resource.Object(bucket_name, complete_path)
+    file_content = json_object.get()['Body'].read().decode('utf-8')
+    json_content = json.loads(file_content)
+    return json_content
 
-        if not dyn_feat:
 
-            json_obj = {"start": str(pd.to_datetime(start_index)),
-                        "target": list(ts.loc[:, target_column])}
-        else:
-            # populating dynamic features array
-            df_dyn_feat = ts.loc[:, dyn_feat]
-            dyn_feat_list = []
-            for feat in df_dyn_feat:
-                dyn_feat_list.append(list(df_dyn_feat[feat]))
+def get_amzn_json_from_s3(s3_resource, bucket_name, prefix=''):
+    """
+    Load AMZN stock json data from S3 resource
+    :param prefix: prefix of the path where the find will be placed
+    :param s3_resource: s3 resource to get data from
+    :param bucket_name: s3 bucket to get data from
+    :return: json object containing IBM stock price adjusted close
+    """
+    amzn_filename = "AMZN.json"
+    complete_path = os.path.join(prefix, amzn_filename)
+    json_object = s3_resource.Object(bucket_name, complete_path)
+    file_content = json_object.get()['Body'].read().decode('utf-8')
+    json_content = json.loads(file_content)
+    return json_content
 
-            # creating json object
-            json_obj = {"start": str(pd.to_datetime(start_index)),
-                        "target": list(ts.loc[:, target_column]),
-                        "dynamic_feat": list(dyn_feat_list)}
 
+def get_aapl_json_from_s3(s3_resource, bucket_name, prefix=''):
+    """
+    Load AAPL stock json data from S3 resource
+    :param prefix: prefix of the path where the file is located
+    :param s3_resource: s3 resource to get data from
+    :param bucket_name: s3 bucket to get data from
+    :return: json object containing Apple Inc. stock price adjusted close
+    """
+    aapl_filename = "AAPL.json"
+    complete_path = os.path.join(prefix, aapl_filename)
+    json_object = s3_resource.Object(bucket_name, complete_path)
+    file_content = json_object.get()['Body'].read().decode('utf-8')
+    json_content = json.loads(file_content)
+    return json_content
+
+
+def get_googl_json_from_s3(s3_resource, bucket_name, prefix=''):
+    """
+    Load GOOGL stock json data from S3 resource
+    :param prefix: prefix of the path where the file is located
+    :param s3_resource: s3 resource to get data from
+    :param bucket_name: s3 bucket to get data from
+    :return: json object containing Alphabet Inc. stock price adjusted close
+    """
+    googl_filename = "GOOGL.json"
+    complete_path = os.path.join(prefix, googl_filename)
+    json_object = s3_resource.Object(bucket_name, complete_path)
+    file_content = json_object.get()['Body'].read().decode('utf-8')
+    json_content = json.loads(file_content)
+    return json_content
+
+
+def get_json_stock_data(ticker_name, s3_resource, s3_bucket):
+    """
+    Retrieves json data from S3
+    :param s3_bucket: the S3 bucket containing the files to be
+    :param s3_resource: the S3 resource to be used to access the file
+    :param ticker_name: ticker name, one among IBM, AAPL, AMZN, GOOGL
+    :param s3_path: a string indicating where to find the json file on S3
+    :return:
+    """
+    if ticker_name.upper() == "IBM":
+        return get_ibm_json_from_s3(s3_resource, s3_bucket)
+    elif ticker_name.upper() == "AMZN":
+        return get_amzn_json_from_s3(s3_resource, s3_bucket)
+    elif ticker_name.upper() == "AAPL":
+        return get_aapl_json_from_s3(s3_resource, s3_bucket)
+    elif ticker_name.upper() == "GOOGL":
+        return get_googl_json_from_s3(s3_resource, s3_bucket)
     else:
-        if not dyn_feat:
-            json_obj = {"start": str(ts.index[0]),
-                        "target": list(ts.loc[:, target_column])}
-
-        else:
-            # populating dynamic features array
-            df_dyn_feat = ts.loc[:, dyn_feat]
-            dyn_feat_list = []
-            for feat in df_dyn_feat:
-                dyn_feat_list.append(list(df_dyn_feat[feat]))
-
-            # creating json object
-            json_obj = {"start": str(ts.index[0]), "target": list(ts.loc[:, target_column]),
-                        "dynamic_feat": list(dyn_feat_list)}
-
-    return json_obj
+        return None
