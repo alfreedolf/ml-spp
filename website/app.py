7c7fe2d756d40c2a5a6f89182eaebbe7ede26597
alfreedolf <2171254+alfreedolf@users.noreply.github.com>
2021-04-12 01:14:01 +0200

    feat: POST method to be called from js function

--- website/app.py
+++ website/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, request, render_template
+from source_deepar.display_quantiles import display_quantiles_flask
+import json
 
 app = Flask(__name__)
 
@@ -9,3 +9,15 @@ def home():
     return render_template(
         "index.html"
     )
+
+
+@app.route('/postmethod', methods = ['POST'])
+def get_post_javascript_data():
+    jsdata = request.form['javascript_data']
+    js_obj_data = json.loads(jsdata)['predictions'][0]['quantiles']
+    quantiles_plot = display_quantiles_flask(js_obj_data)
+    return quantiles_plot
+
+@app.route('/getmethod/<jsdata>')
+def get_javascript_data(jsdata):
+    return json.loads(jsdata)[0]
