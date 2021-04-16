5fd3283d221a4701f87d1c48bde9ebd769e215d7
alfreedolf <2171254+alfreedolf@users.noreply.github.com>
2021-04-16 23:03:11 +0200

    feat: plot_benchmark complete

--- website/app.py
+++ website/app.py
@@ -18,9 +18,15 @@ def home():
 def predict_from_data():
     # retrieving data to be used as ground truth
     ticker_name = request.form['ticker_name']
-    dataset = request.form['dataset']
-    gt_dict = get_ground_truth_from_s3(ticker_name, dataset)
+    req_dataset = request.form['dataset']
+    gt_dict = get_stock_data_from_s3_bucket(ticker_name, req_dataset)
+    # retrieving ts start date
     start_date = gt_dict['start']
+    # retrieving target ts data
+    target_ts = gt_dict['target']
+    # retrieving benchmark data
+    bk_dict = get_stock_data_from_s3_bucket(ticker_name, 'benchmark')
+    benchmark_ts = bk_dict['target']
 
     # retrieving data to be used for prediction
     js_pred_data = request.form['predicted_data']
@@ -28,12 +34,12 @@ def predict_from_data():
     quantiles_dict = pred_dict[0]['quantiles']
 
     # displaying quantiles graph
-    qp = display_quantiles_flask(quantiles_dict, target_ts=gt_dict['target'],
-                                 start=start_date)
+    qp = display_quantiles_flask(quantiles_dict, target_ts=target_ts, bench_mark_prediction=benchmark_ts,
+                                 bench_mark_prediction_name='SMA', start=start_date)
     return qp
 
 
-def get_ground_truth_from_s3(ticker_name, dataset):
+def get_stock_data_from_s3_bucket(ticker_name, dataset):
     # S3 resource invocation
     s3_resource = boto3.resource('s3')
     # S3 bucket selection
