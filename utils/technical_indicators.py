4f6760f5d8428220106f59cf9dbb08e515e7bcec
alfreedolf <2171254+alfreedolf@users.noreply.github.com>
2021-05-04 23:40:37 +0200

    chore: fix to technical_indicators.py

--- utils/technical_indicators.py
+++ utils/technical_indicators.py
@@ -9,7 +9,7 @@ import pandas as pd
 # moving average
 def moving_average(time_series, window_size=20, fwd_fill_to_end=0):
     """
-    A Simple Moving Average (SMA) function
+    Computes a Simple Moving Average (SMA) function on a time series
     :param time_series: a pandas time series input containing numerical values
     :param window_size: a window size used to compute the SMA
     :param fwd_fill_to_end: index from which computation must stop and propagate last value
@@ -24,24 +24,48 @@ def moving_average(time_series, window_size=20, fwd_fill_to_end=0):
 
 
 # standard deviation
-def std_dev(stock_price_time_series, window_size=20, fwd_fill_to_end=0):
-    return stock_price_time_series.rolling(window=window_size).std()
+def std_dev(time_series, window_size=20, fwd_fill_to_end=0):
+    """
+    Computes Standard Deviation (STD) function on a time series
+    :param time_series: a pandas input time series containing numerical values
+    :param window_size: a window size used to compute the STD
+    :param fwd_fill_to_end: index from which computation must stop and propagate last value
+    :return: Standard Deviation time series
+    """
+    if fwd_fill_to_end <= 0:
+        return time_series.rolling(window=window_size).std()
+    else:
+        std = time_series.rolling(window=window_size).std()
+        std[-fwd_fill_to_end:] = std.iloc[-fwd_fill_to_end]
+        return std
 
 
 # stock price volatility
-def volatility(stock_price_time_series, window_size=20, fwd_fill_to_end=0):
-    sv = std_dev(stock_price_time_series, window_size=window_size)
+def volatility(time_series, window_size=20):
+    """
+    Computes volatility on a time series
+    :param time_series: a pandas input time series containing numerical values
+    :param window_size: a window size used to compute the volatility value
+    :return: volatility of the time series values
+    """
+    sv = std_dev(time_series, window_size=window_size)
     vol = sum(np.square(sv.values[window_size - 1:])) / len(sv.values)
     return vol
 
 
 # bollinger bands
 def bollinger_bands(stock_price_time_series, window_size=20, num_of_std=2, fwd_fill_to_end=0):
-    rolling_mean = moving_average(stock_price_time_series, window_size=window_size)
-    rolling_std = std_dev(stock_price_time_series, window_size=window_size)
+    """
+    Computes Bollinger bands function values on a time series
+    :param stock_price_time_series:
+    :param window_size:
+    :param num_of_std:
+    :param fwd_fill_to_end:
+    :return: moving average time series, bollinger upper band, bollinger lower band
+    """
+    rolling_mean = moving_average(stock_price_time_series, window_size=window_size, fwd_fill_to_end=fwd_fill_to_end)
+    rolling_std = std_dev(stock_price_time_series, window_size=window_size, fwd_fill_to_end=fwd_fill_to_end)
     upper_band = rolling_mean + (rolling_std * num_of_std)
     lower_band = rolling_mean - (rolling_std * num_of_std)
 
     return rolling_mean, upper_band, lower_band
-
-# momentum
